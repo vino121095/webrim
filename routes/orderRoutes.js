c1248b46bb7d946714b9e575fad888b76d8d4d4b
Selvaganesh K <selvaganesh2608@gmail.com>
2025-01-28 12:31:38 +0530

    Latesh changes | notification corrections | 28-01

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,22 +3,22 @@ const express = require('express');
 const router = express.Router();
 const OrderController = require('../controller/Ordercontroller');  // Ensure correct casing and path
 const { orderValidator } = require('../valitadors/OrderValidator');
-const {generateOrderId, addOrderDate} = require('../middlewares/generateUinqueId');
+const { generateOrderId, addOrderDate } = require('../middlewares/generateUinqueId');
+
 // Route to place an order with validation
-router.post('/placeOrder', generateOrderId, addOrderDate,  orderValidator, OrderController.createOrder);
+router.post('/placeOrder', generateOrderId, addOrderDate, orderValidator, OrderController.createOrder);
 
+// Order management routes
 router.put('/cancelOrder/:id', OrderController.cancelOrder);
-
 router.put('/completeOrder/:id', OrderController.completeOrder);
 
-// Route to get all orders
+// Order retrieval routes
 router.get('/orders', OrderController.getAllOrders);
-
 router.get('/ordersfornotify', OrderController.getAllOrdersForNotify);
-
-// Route to get orders by user ID
 router.get('/userOrdersById/:id', OrderController.getOrdersByUserId);
-
 router.get('/order/:id', OrderController.getOrderById);
+
+// Notification route
+router.get('/notifications/:userId', OrderController.getOrderNotifications);
 
 module.exports = router;
