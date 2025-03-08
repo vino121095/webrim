17623d865fea7c6af663f55a5c8bb539e7e7cadb
Selvaganesh K <selvaganesh2608@gmail.com>
2025-03-08 10:13:29 +0530

    Latest changes for pwa

--- src/index.js
+++ src/index.js
@@ -14,4 +14,15 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Register the Service Worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((reg) => console.log("Service Worker Registered", reg))
+      .catch((err) => console.log("Service Worker Registration Failed", err));
+  });
+}
+
+// Performance Monitoring
 reportWebVitals();
